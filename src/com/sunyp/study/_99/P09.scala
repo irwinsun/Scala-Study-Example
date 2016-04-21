58a7c99512bd6a0e5f65584d792d806b69e41e27
weststorm.syp <sunyipingapple@gmail.com>
2016-04-21 22:23:24 +0800

    打包列表

---
+++ src/com/sunyp/study/_99/P09.scala
@@ -0,0 +1,33 @@
+package com.sunyp.study._99
+
+/**
+  * Created by sunyp on 16/4/21.
+  *
+  * Pack consecutive duplicates of list elements into sublists.
+  * If a list contains repeated elements they should be placed in separate sublists.
+  * Example:
+  * scala> pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  * res0: List[List[Symbol]] = List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+  */
+object P09 extends App {
+
+
+  val lst = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
+
+  def pack[A](ls: List[A]): List[List[A]] = {
+    if (ls == null || ls.isEmpty)
+      List(List())
+    else {
+      val (headLst, next) = ls span {
+        _ == ls.head
+      }
+      if (next == Nil)
+        List(headLst)
+      else
+        headLst :: pack(next)
+    }
+  }
+
+  println(s"pack($lst)=${pack(lst)}")
+}
